d8464a23c0cafd12723ac3d702134fdb56a611f4
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-11-05 23:49:00 -0600

    update

--- FinancialDisplayer.py
+++ FinancialDisplayer.py
@@ -1,33 +1,30 @@
-import pandas as pd
 import csv
 import matplotlib.pyplot as plt
+from .errors
 
 class BankCSV:
     def __init__(self) -> None:
         pass
 
     # Read bank CSV and return a list of strings and numbers.
-    def readBankCSV(self, fileName: str) -> None:
-        csvData = []
-
-        with open(f"./csv/{fileName}", "r") as csvFile:
-            csvData = list(csv.reader(csvFile))
-        return csvData
+    def readBankCSV(self, fileName: str) -> list:
+        try:
+            with open(f"./csv/{fileName}", "r") as csvFile:
+                csvData = list(csv.reader(csvFile))
+                
+                return csvData
+        except FileNotFoundError:
+            print("File was not found, did you forget the .csv?")
     
     # Display CSV data and return nothing.
-    def displayBankCSV(self, bankData: list, x: str, y: str) -> None: 
-        df = pd.DataFrame(bankData)
-        plt.plot(df[x], df[y])
-        plt.show()
-    
-    # Export CSV data if somehow stuff got changed.
-    def exportBankCSV(self):
-        raise NotImplementedError()
+    def displayBankCSV(self, bankData: list, x: str, y: str) -> None:
+        xValues, yValues = []
 
-def main() -> None:
-    bankCSV = BankCSV()
-    bankData = bankCSV.readBankCSV("bankinfo.CSV")
-    bankCSV.displayBankCSV(bankData, 1, 5)
+        if x in bankData[0] and y in bankData[0]:
+            for list in bankData:
+                xValues.append(list[bankData[0].index(x)])
+                yValues.append(list[bankData[0].index(y)])
+        else:
+            r
 
-if __name__ == "__main__":
-    main()
+        print(xValues)
